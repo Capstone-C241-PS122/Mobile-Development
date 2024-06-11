9289b775e5e87b7c979013da7ff052c51b7a240a
filipia xena <100193047+filipia112@users.noreply.github.com>
2024-06-11 13:58:41 +0700

    create list_item history,library video, article.Revisi Details and MainMenu

--- app/src/main/java/com/grassterra/fitassist/HomeFragment.kt
+++ app/src/main/java/com/grassterra/fitassist/HomeFragment.kt
@@ -10,30 +10,12 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.grassterra.fitassist.databinding.FragmentHomeBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-
-    private var param1: String? = null
-    private var param2: String? = null
-
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -89,14 +71,5 @@ class HomeFragment : Fragment() {
         _binding = null
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
--- app/src/main/java/com/grassterra/fitassist/ui/DetailExerciseActivity.kt
+++ app/src/main/java/com/grassterra/fitassist/ui/DetailExerciseActivity.kt
@@ -4,16 +4,17 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
+import android.view.View
+import android.view.animation.AlphaAnimation
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.NestedScrollView
 import com.grassterra.fitassist.databinding.ActivityDetailExerciseBinding
 import com.grassterra.fitassist.helper.ParcelableMap
 
 class DetailExerciseActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailExerciseBinding
+    private var isTopBarVisible = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,7 +23,27 @@ class DetailExerciseActivity : AppCompatActivity() {
         binding.btnBack.setOnClickListener{
             BackMainMenu(this)
         }
+        binding.scrollView.setOnScrollChangeListener(NestedScrollView.OnScrollChangeListener { _, _, scrollY, _, _ ->
+            if (scrollY > 0 && binding.topBar.visibility != View.VISIBLE) {
+                fadeInView(binding.topBar)
+            } else if (scrollY == 0 && binding.topBar.visibility == View.VISIBLE) {
+                fadeOutView(binding.topBar)
+            }
+        })
         setupImage()
+    }
+    private fun fadeInView(view: View) {
+        val anim = AlphaAnimation(0.0f, 1.0f)
+        anim.duration = 500
+        view.visibility = View.VISIBLE
+        view.startAnimation(anim)
+    }
+
+    private fun fadeOutView(view: View) {
+        val anim = AlphaAnimation(1.0f, 0.0f)
+        anim.duration = 500
+        view.startAnimation(anim)
+        view.visibility = View.INVISIBLE
     }
     private fun BackMainMenu(context: Context){
         val intent = Intent(context,MainMenu::class.java)
