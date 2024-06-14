27c57e8d1a2b92a057b7dcec660db49fca1546e3
filipia xena <100193047+filipia112@users.noreply.github.com>
2024-06-14 22:44:13 +0700

    Design Layout dan Integrasi API Nutrition

--- app/src/main/java/com/grassterra/fitassist/ui/ActivityCalculateNutrition.kt
+++ app/src/main/java/com/grassterra/fitassist/ui/ActivityCalculateNutrition.kt
@@ -1,13 +1,18 @@
 package com.grassterra.fitassist.ui
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.grassterra.fitassist.databinding.ActivityCalculateNutritionBinding
-import java.text.DecimalFormat
-import java.util.Locale
+import com.grassterra.fitassist.retrofit.ApiConfig
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
 
 class ActivityCalculateNutrition : AppCompatActivity() {
     private lateinit var binding: ActivityCalculateNutritionBinding
+    private val apiService = ApiConfig.getApiService()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCalculateNutritionBinding.inflate(layoutInflater)
@@ -18,58 +23,25 @@ class ActivityCalculateNutrition : AppCompatActivity() {
         }
     }
     private fun resultCalculate() {
-        var kalori100gram = 0.0
-        var protein100gram = 0.0
-        var karbohidrat100gram = 0.0
-        var lemak100gram = 0.0
-        val df = DecimalFormat("#.##")
-        val editTextFood = binding.editTextFood.text.toString()
-
-        when (editTextFood.toLowerCase(Locale.getDefault())) {
-            "dada ayam" -> {
-                kalori100gram = 195.0
-                protein100gram  = 30.0
-                karbohidrat100gram = 0.0
-                lemak100gram = 8.0
-            }
-            "wortel" -> {
-                kalori100gram  = 41.0
-                protein100gram = 0.93
-                karbohidrat100gram = 9.58
-                lemak100gram = 0.24
-            }
-
-            else -> {
-                binding.textViewResult.text = "Makanan tidak ditemukan."
-                return
+        val foodName = binding.editTextFood.text.toString().trim()
+        val foodWeight = binding.editTextWeight.text.toString().toIntOrNull() ?: 0
+        CoroutineScope(Dispatchers.Main).launch {
+            try {
+                apiService.postNutrition(name = foodName,weight = foodWeight)
+                showToast("Data sent successfully")
+            } catch (e: Exception) {
+                if (e is HttpException) {
+                    showToast("Error: ${e.message()}")
+                } else {
+                    showToast("Error: ${e.message}")
+                }
+            } finally {
+                binding.editTextFood.text.clear()
+                binding.editTextWeight.text.clear()
             }
         }
-        val makananpergram = binding.editTextWeight.text.toString().toInt()
-        val dibagiPergramKalori = kalori100gram / 100
-        val dibagiPergramProtein = protein100gram / 100
-        val dibagiPergramLemak = lemak100gram / 100
-        val dibagiPergramKarbohidrat = karbohidrat100gram / 100
-        val hasilkalori = makananpergram * dibagiPergramKalori
-        val hasilprotein = makananpergram * dibagiPergramProtein
-        val hasilkarbohidrat = makananpergram * dibagiPergramKarbohidrat
-        val hasillemak = makananpergram * dibagiPergramLemak
-
-        val formattedKalori = df.format(hasilkalori)
-        val formattedProtein = df.format(hasilprotein)
-        val formattedKarbohidrat = df.format(hasilkarbohidrat)
-        val formattedLemak = df.format(hasillemak)
-
-        binding.textViewResult.text = "===== Informasi Gizi =====\n" +
-                "Makanan: $editTextFood\n" +
-                "Berat: $makananpergram gram\n" +
-                "Kalori: $formattedKalori Kalori\n" +
-                "Protein: $formattedProtein gram\n" +
-                "Karbohidrat: $formattedKarbohidrat gram\n" +
-                "Lemak: $formattedLemak gram"
-
-        binding.editTextFood.text.clear()
-        binding.editTextWeight.text.clear()
     }
-
-
+    private fun showToast(message: String) {
+        Toast.makeText(this@ActivityCalculateNutrition, message, Toast.LENGTH_SHORT).show()
+    }
 }
